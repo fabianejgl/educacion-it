7584b2904d462ce963e1ebef1e1f32704a5dd2ae
Fabian Gomez <fabiangl2011@gmail.com>
2023-07-20 16:21:58 -0300

    Primer push

---
+++ M2-html-avanzado/clase-04/clase-04-profe/script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mkdir css js
+touch index.html README.md css/estilos.css js/main.js
+git init
+git add .
+git commit -m "Commit inicial"
---
+++ M2-html-avanzado/clase-04/init-vite-main/vite.config.js
@@ -0,0 +1,9 @@
+
+export default {
+    server: {
+        port:'2222', //5173 puerto por defecto
+    },
+    css: {
+        devSourceMap: true,
+    }
+}
